5cfddbb3cdf3ffe50a48d5f7e08a20f6ae7db206
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-05-05 20:49:03 +0500

    ""

--- app/components/Home/Skills/SkillCards.jsx
+++ app/components/Home/Skills/SkillCards.jsx
@@ -6,11 +6,17 @@ function Skills() {
 		{ id: 4, name: 'JavaScript', image: 'javadcript.png' },
 		{ id: 5, name: 'TypeScript', image: 'ts.jpg' },
 		{ id: 6, name: 'Git', image: 'git.png' },
+		{ id: 6, name: 'Tailwind css', image: 'tailwind.png' },
+		{ id: 6, name: 'Bootstrap', image: 'bootstrap.png' },
+		{ id: 6, name: 'React js', image: 'React.webp' },
+		{ id: 6, name: 'Next js', image: 'next.png' },
 	]
 
 	return (
 		<section className='max-w-[1100px] mx-auto py-16 px-4'>
-			<h2 className='text-white text-center text-4xl font-bold mb-12'>My Skills</h2>
+			<h2 className='text-white text-center text-4xl font-bold mb-12'>
+				My Skills
+			</h2>
 			<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6'>
 				{skills.map(skill => (
 					<div
